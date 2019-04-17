9a5c95639faa866a9681d1eab4a05c93adbbb786
shawnle <shawn_le@solomon.com.tw>
2019-04-17 18:13:32 +0800

    more on data loading flow

--- lib/datasets/lov_syn_dataset.py
+++ lib/datasets/lov_syn_dataset.py
@@ -1,21 +1,65 @@
+import sys
+
+sys.path.append('.')
+sys.path.append('..')
+
 import os
 import torch
 import torch.utils.data as data
 
+from lib.utils import data_utils
+
 
 class lov_syn_dataset(data.Dataset):
+    """ 
+        just a light wrapper of LovSynImageDB to become data.Dataset
+    """
 
     def __init__(self, path):
-        self.dataset_name = 'lov_syn'
 
+        self.dataset_name = 'lov_syn'
+        self.lov_data =  data_utils.LovSynImageDB()
 
 
     def __len__(self):
 
-        return 0
+        return self.lov_data.num_images
 
 
     def __getitem__(self, index):
 
-        return None
+        return self.lov_data.roidb[index]
 
+
+
+class lov_syn_DatasetRealAug(data.Dataset):
+    """
+        this adds augmentation information to image and meta data
+    """
+
+    def __init__(self, imageDB):
+
+        self._imageDB = imageDB
+        self._aug_db = do_augment()
+        self.data_prefix = ''
+
+    def __len__(self):
+
+        return self._imageDB.lov_data.num_images
+
+
+    def __getitem__(self, index):
+
+        rgb_path = os.path.join(self.data_prefix, self._imageDB.lov_data.roidb[index]['image'])
+        mask_path = os.path.join(self.data_prefix, self._imageDB.lov_data.roidb[index]['depth'])  # check
+
+        
+
+        return _aug_db[index]
+
+
+    def do_augment(self):
+
+
+
+        return aug_db
